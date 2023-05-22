922026486cbc6216b8c3389cd70ee9b65668bba2
JAHZRMX <112454702+JAHZRMX@users.noreply.github.com>
2023-05-22 15:49:49 -0600

    Add files via upload

--- proyecto-filtros/main.js
+++ proyecto-filtros/main.js
@@ -1,16 +1,40 @@
-function preload(){
+noseX=0;
+noseY=0;
 
+function preload() {
+  clown_nose = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
 }
 
-function setup(){
-    canvas = createCanvas(300,300);
-    canvas.center();
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(300, 300);
+  video.hide();
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
 }
 
-function draw(){
-
+function modelLoaded() {
+  console.log('PoseNet está inicializada');
 }
 
-function take_snapshot(){
-    Save('my_image.png');
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
+    noseX = results[0].pose.nose.x-40;
+    noseY = results[0].pose.nose.y;
+  }
+}
+
+function draw() {
+  image(video, 0, 0, 300, 300);
+  image(clown_nose, noseX, noseY, 80, 35);
+}
+
+function take_snapshot(){    
+  save('myFilterImage.png');
 }
